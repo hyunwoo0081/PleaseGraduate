a4c42e7cc910e946f9c527e554461c396290a30b
hyunwoo0081 <hyunwoo0081@gmail.com>
2026-06-30 20:46:45 +0900

    fix: set django settings module and fix allowed hosts for render

--- docker_cmd.sh
+++ docker_cmd.sh
@@ -7,7 +7,8 @@ PROFILE=$1
 if [ -n "$PORT" ]; then
     # Render와 같이 PORT 환경 변수가 주어지는 플랫폼용 설정 (HTTP 프로토콜 사용)
     echo "Running on Render/Cloud platform with PORT=$PORT"
-    uwsgi --http :$PORT --module project.wsgi:application --master --processes 4 --threads 2
+    export DJANGO_SETTINGS_MODULE=project.settings.prod
+    uwsgi --http :$PORT --module project.wsgi:application --master --processes 4 --threads 2 --buffer-size 65535
 elif [ "$PROFILE" == "dev" ]; then
     cd /srv/PleaseGraduate/dev/
     uwsgi --ini uwsgi.ini
